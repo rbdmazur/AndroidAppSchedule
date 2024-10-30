bd84ce957d924600e9b4c76ddfb179dddda96d06
Daniil Zhuk <rbdmazur@gmail.com>
2024-10-30 23:30:22 +0300

    Bottom Navigation View was added

--- app/src/main/java/com/example/schedule/MainActivity.kt
+++ app/src/main/java/com/example/schedule/MainActivity.kt
@@ -5,10 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.schedule.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = checkNotNull(_binding)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val host =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navController = host.navController
+
+        val navView = binding.bottomNavView
+
+        navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener {_, _, arguments ->
+            navView.isVisible = arguments?.getBoolean("hideAppBar", false) != true
+        }
     }
 }
